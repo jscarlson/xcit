79b88752a46ccfa4659532f9fa49d8679e0054a0
jscarlson <jcarlson238@gmail.com>
2021-12-30 15:22:17 -0500

    debug

--- detection/backbone/xcit.py
+++ detection/backbone/xcit.py
@@ -394,7 +394,6 @@ class XCiT(nn.Module):
 
     def init_weights(self, pretrained=None):
         """Initialize the weights in backbone.
-
         Args:
             pretrained (str, optional): Path to pre-trained weights.
                 Defaults to None.
@@ -409,14 +408,13 @@ class XCiT(nn.Module):
                 nn.init.constant_(m.bias, 0)
                 nn.init.constant_(m.weight, 1.0)
 
-        if isinstance(pretrained, str):
-            self.apply(_init_weights)
-            logger = get_root_logger()
-            load_checkpoint(self, pretrained, strict=False, logger=logger)
-        elif pretrained is None:
-            self.apply(_init_weights)
-        else:
-            raise TypeError('pretrained must be a str or None')
+        pretrained = 'pretrained/xcit_small_12_cp16_dino.pth'
+        print("Loading pretrained weights from checkpoint", pretrained)
+        checkpoint = torch.load(pretrained, map_location='cpu')
+
+        checkpoint_model = checkpoint['model']
+        state_dict = self.state_dict()
+        self.load_state_dict(checkpoint_model, strict=False)
 
     def forward_features(self, x):
         B, C, H, W = x.shape
