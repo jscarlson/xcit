a14195b05527eb349105d281cda2753b614454ad
jscarlson <jcarlson238@gmail.com>
2022-01-04 02:53:43 -0500

    revert

--- detection/tools/test.py
+++ detection/tools/test.py
@@ -125,8 +125,6 @@ def main():
     cfg = Config.fromfile(args.config)
     if args.cfg_options is not None:
         cfg.merge_from_dict(args.cfg_options)
-        s = pprint.pformat(cfg)
-        print(s)
     # import modules from string list.
     if cfg.get('custom_imports', None):
         from mmcv.utils import import_modules_from_strings
