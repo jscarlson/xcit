95ff9ec5b750a39b296e7f3b8bb8ba4fc84187b3
jscarlson <jcarlson238@gmail.com>
2022-04-12 11:07:29 -0400

    dev

--- detection/configs/_base_/datasets/custom_detection.py
+++ detection/configs/_base_/datasets/custom_detection.py
@@ -1,8 +1,7 @@
 # Copyright (c) 2015-present, Facebook, Inc.
 # All rights reserved.
 dataset_type = 'CocoDataset'
-data_root = '/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/Label-Studio-Labeling/pr-non-title-character-detection/combined-output/'
-# data_root = '/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/Label-Studio-Labeling/newspaper-character-labeling/combined-output/'
+data_root = '/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/synthtxt_simpler_more/'
 classes = ('character',)
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
@@ -36,23 +35,21 @@ data = dict(
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
+        # explicitly add your class names to the field `classes`
         classes=classes,
-        ann_file=data_root + 'input/train80_fixed.json',
-        # ann_file=data_root + 'input/train80.json',
-        img_prefix=data_root + 'generated/',
-        pipeline=train_pipeline),
+        ann_file=data_root+'train90.json',
+        img_prefix=data_root+'images/'),
     val=dict(
         type=dataset_type,
+        # explicitly add your class names to the field `classes`
         classes=classes,
-        ann_file=data_root + 'input/test20_fixed.json',
-        # ann_file=data_root + 'input/test20.json',
-        img_prefix=data_root + 'generated/',
-        pipeline=test_pipeline),
+        ann_file=data_root+'test10.json',
+        img_prefix=data_root+'images/'),
     test=dict(
         type=dataset_type,
+        # explicitly add your class names to the field `classes`
         classes=classes,
-        ann_file=data_root + 'input/test20_fixed.json',
-        # ann_file=data_root + 'input/test20.json',
-        img_prefix=data_root + 'generated/',
-        pipeline=test_pipeline))
-evaluation = dict(interval=1, metric='bbox')
+        ann_file=data_root+'test10.json',
+        img_prefix=data_root+'images/'))
+
+evaluation = dict(metric=['bbox', 'segm'], save_best='bbox_mAP', classwise=True)
