34a1f2a8f4e95db2fb0b4fb9c96c487866c58789
jscarlson <jcarlson238@gmail.com>
2022-04-12 18:56:31 -0400

    dev

--- detection/configs/_base_/datasets/custom_detection_finetune.py
+++ detection/configs/_base_/datasets/custom_detection_finetune.py
@@ -20,7 +20,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1333, 64),
+        img_scale=(64, 1333),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
