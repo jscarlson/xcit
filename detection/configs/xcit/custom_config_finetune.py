c34a61179d5480033c682e60291eb80151133016
jscarlson <jcarlson238@gmail.com>
2022-04-12 21:36:34 -0400

    dev

--- detection/configs/xcit/custom_config_finetune.py
+++ detection/configs/xcit/custom_config_finetune.py
@@ -100,7 +100,7 @@ log_config = dict(
             type='WandbLoggerHook',
             init_kwargs=dict(
                 project='mmdetection',
-                name='xcit-s-eng-finetune-allnews'))
+                name='xcit-s-eng-finetune-burlington'))
     ])
 
 # do not use mmdet version fp16
