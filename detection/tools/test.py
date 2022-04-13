2ca7fa2947a1e51a7230c3f9b689627af7d70d9e
jscarlson <jcarlson238@gmail.com>
2022-04-12 20:36:03 -0400

    dev

--- detection/tools/test.py
+++ detection/tools/test.py
@@ -23,8 +23,8 @@ from mmdet.datasets import (build_dataloader, build_dataset,
                             replace_ImageToTensor)
 from mmdet.models import build_detector, build_backbone
 
-sys.path.append("../backbone")
-from backbone import xcit
+sys.path.insert(0, "../backbone")
+import xcit
 
 
 def parse_args():
