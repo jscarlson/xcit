5346bec7fa44ded70c0494170041d746779f7aff
jscarlson <jcarlson238@gmail.com>
2022-05-10 11:17:50 -0400

    dev

--- detection/configs/_base_/models/custom_cascade_twoclass.py
+++ detection/configs/_base_/models/custom_cascade_twoclass.py
@@ -92,19 +92,7 @@ model = dict(
                     loss_weight=1.0),
                 loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))
         ],
-        mask_roi_extractor=dict(
-            type='SingleRoIExtractor',
-            roi_layer=dict(type='RoIAlign', output_size=14, sampling_ratio=0),
-            out_channels=256,
-            featmap_strides=[4, 8, 16, 32]),
-        mask_head=dict(
-            type='FCNMaskHead',
-            num_convs=4,
-            in_channels=256,
-            conv_out_channels=256,
-            num_classes=2,
-            loss_mask=dict(
-                type='CrossEntropyLoss', use_mask=True, loss_weight=1.0))),
+    ),
     # model training and testing settings
     train_cfg=dict(
         rpn=dict(
@@ -144,7 +132,6 @@ model = dict(
                     pos_fraction=0.25,
                     neg_pos_ub=-1,
                     add_gt_as_proposals=True),
-                mask_size=28,
                 pos_weight=-1,
                 debug=False),
             dict(
@@ -161,7 +148,6 @@ model = dict(
                     pos_fraction=0.25,
                     neg_pos_ub=-1,
                     add_gt_as_proposals=True),
-                mask_size=28,
                 pos_weight=-1,
                 debug=False),
             dict(
@@ -178,7 +164,6 @@ model = dict(
                     pos_fraction=0.25,
                     neg_pos_ub=-1,
                     add_gt_as_proposals=True),
-                mask_size=28,
                 pos_weight=-1,
                 debug=False)
         ]),
@@ -191,5 +176,4 @@ model = dict(
         rcnn=dict(
             score_thr=0.05,
             nms=dict(type='nms', iou_threshold=0.5),
-            max_per_img=100,
-            mask_thr_binary=0.5)))
+            max_per_img=100,)))
