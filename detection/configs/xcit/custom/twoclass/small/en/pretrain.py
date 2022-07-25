7e7e6c919ab3c53fc35f3a56c24f2a715cb7a612
jscarlson <jcarlson238@gmail.com>
2022-07-25 11:45:49 -0700

    dev

--- detection/configs/xcit/custom/twoclass/small/en/pretrain.py
+++ detection/configs/xcit/custom/twoclass/small/en/pretrain.py
@@ -6,10 +6,10 @@ https://github.com/SwinTransformer/Swin-Transformer-Object-Detection
 """
 
 _base_ = [
-    '/srv/ocr/github_repos/xcit/detection/configs/_base_/models/custom_model_twoclass.py',
-    '/srv/ocr/github_repos/xcit/detection/configs/_base_/datasets/en/custom_detection_twoclass.py',
-    '/srv/ocr/github_repos/xcit/detection/configs/_base_/schedules/schedule_1x.py',
-    '/srv/ocr/github_repos/xcit/detection/configs/_base_/default_runtime.py'
+    '/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/github_repos/xcit/detection/configs/_base_/models/custom_model_twoclass.py',
+    '/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/github_repos/xcit/detection/configs/_base_/datasets/en/custom_detection_twoclass.py',
+    '/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/github_repos/xcit/detection/configs/_base_/schedules/schedule_1x.py',
+    '/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/github_repos/xcit/detection/configs/_base_/default_runtime.py'
 ]
 
 model = dict(
