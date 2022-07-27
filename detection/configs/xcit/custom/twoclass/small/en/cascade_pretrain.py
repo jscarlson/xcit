cd01e2db2106c8bfd33d7c6124d67d3cf8b54da8
jscarlson <jcarlson238@gmail.com>
2022-07-26 17:04:25 -0700

    dev

--- detection/configs/xcit/custom/twoclass/small/en/cascade_pretrain.py
+++ detection/configs/xcit/custom/twoclass/small/en/cascade_pretrain.py
@@ -6,10 +6,10 @@ https://github.com/SwinTransformer/Swin-Transformer-Object-Detection
 """
 
 _base_ = [
-    '/srv/ocr/github_repos/xcit/detection/configs/_base_/models/custom_cascade_twoclass.py',
-    '/srv/ocr/github_repos/xcit/detection/configs/_base_/datasets/custom_detection_twoclass.py',
-    '/srv/ocr/github_repos/xcit/detection/configs/_base_/schedules/schedule_1x.py', 
-    '/srv/ocr/github_repos/xcit/detection/configs/_base_/default_runtime.py'
+    '/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/github_repos/xcit/detection/configs/_base_/models/custom_cascade_twoclass.py',
+    '/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/github_repos/xcit/detection/configs/_base_/datasets/en/custom_detection_twoclass.py',
+    '/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/github_repos/xcit/detection/configs/_base_/schedules/schedule_1x.py', 
+    '/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/github_repos/xcit/detection/configs/_base_/default_runtime.py'
 ]
 
 model = dict(
@@ -86,7 +86,7 @@ log_config = dict(
         dict(
             type='WandbLoggerHook',
             init_kwargs=dict(
-                project='mmdetection',
+                project='effocr_loc_v1',
                 name='xcit-m-eng-pretrain'))
     ])
 
